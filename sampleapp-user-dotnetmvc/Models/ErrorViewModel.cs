b2eb0cbe1890af58d47f0d77828ab39a0afd6298
Muham <Muham@DESKTOP-BJ2G5D7>
2024-01-28 01:38:29 +0330

    Add project files.

---
+++ sampleapp-user-dotnetmvc/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace sampleapp_user_dotnetmvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ sampleapp-user-dotnetmvc/Models/Person.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace sampleapp_user_dotnetmvc.Models
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+
+    public class PersonContext : DbContext
+    {
+        public PersonContext(DbContextOptions options) : base(options)
+        {
+           
+        }
+
+        public DbSet<Person> People { get; set; }
+    }
+}
